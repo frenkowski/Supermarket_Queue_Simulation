2f45314df0f156814a884281f087c25eba558d18
Francesco Stranieri <frenkowski@frenkowskis-MacBook-Pro.local>
2020-07-01 15:33:11 +0200

    Added initial code for Agent

--- src/model.py
+++ src/model.py
@@ -22,13 +22,14 @@ class CashierAgent(Agent):
 
 class CustomerAgent(Agent):
     """ An agent with fixed initial wealth."""
+
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
 
     def step(self):
         # The agent's step will go here.
         # For demonstration purposes we will print the agent's unique_id
-        print ("Hi, I am agent " + str(self.unique_id) +".")
+        print("Hi, I am agent " + str(self.unique_id) + ".")
 
 
 class SupermarketModel(Model):
@@ -38,6 +39,10 @@ class SupermarketModel(Model):
         self.grid = SingleGrid(width, height, True)
         self.schedule = RandomActivation(self)
 
+        self.entry_point = (0, 0)
+        self.capacity = 50
+        self.agents_count = 0
+
         # Populate grid from world
         for i, row in enumerate(self.world):
             for j, cell in enumerate(row):
@@ -45,6 +50,9 @@ class SupermarketModel(Model):
                     self.grid[j][i] = ObstacleAgent(str(i)+str(j), self)
                 if (cell == 'C'):
                     self.grid[j][i] = CashierAgent(str(i)+str(j), self)
+                if (cell == 'E'):
+                    self.entry_point = (j, i)
+                    print(self.entry_point)
 
         # # Create agents
         # for i in range(self.num_agents):
@@ -59,4 +67,16 @@ class SupermarketModel(Model):
         #     self.grid.place_agent(a, (x, y))
 
     def step(self):
+        # for i in range(self.num_agents):
+        if (self.schedule.agents < self.capacity):
+            a = createAgent()
+            self.schedule.add(a)
+            if(not self.grid.is_cell_empty(self.entry_point)):
+                self.grid.place_agent(a, self.entry_point)
+
         self.schedule.step()
+
+    def createAgent(self):
+        a = CustomerAgent(self.agents_count, self)
+        self.agents_count += 1
+        return a
--- src/server.py
+++ src/server.py
@@ -4,7 +4,7 @@ from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 import numpy as np
 
-from model import SupermarketModel, ObstacleAgent, CashierAgent
+from model import SupermarketModel, ObstacleAgent, CashierAgent, CustomerAgent
 
 
 def agent_portrayal(agent):
@@ -28,7 +28,18 @@ def agent_portrayal(agent):
             "Color": "green" if agent.open else "red"
         }
 
+    if isinstance(agent, CustomerAgent):
+        return {
+            "Shape": "rect",
+            "Filled": "true",
+            "Layer": 0,
+            "w": 0.95,
+            "h": 0.95,
+            "Color": "blue"
+        }
+
     return {}
+
 
 with open(os.path.join(os.getcwd(), '../resources', 'map1.txt')) as f:
     world = [list(c) for c in f.read().split('\n')[::-1] if c]
@@ -40,6 +51,6 @@ grid = CanvasGrid(agent_portrayal, width, height, width*40, height*40)
 server = ModularServer(SupermarketModel,
                        [grid],
                        "Supermarket Model",
-                       {"N":10, "world": world, "width": width, "height": height})
-server.port = 8521 # The default
+                       {"N": 10, "world": world, "width": width, "height": height})
+server.port = 8521  # The default
 server.launch()

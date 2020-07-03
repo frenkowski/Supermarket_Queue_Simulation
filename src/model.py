d872ab8687313865ce63a54ac66aa3ea9bfc08b2
Francesco Stranieri <frenkowski@frenkowskis-MacBook-Pro.local>
2020-07-03 16:45:10 +0200

    Added Agent Movement

--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.space import SingleGrid
+
 import numpy as np
 from scipy.spatial import distance
 
@@ -26,12 +27,33 @@ class CustomerAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.permanence_time = self.random.randint(0, 4)
+        self.spawn = True
 
     def step(self):
         # The agent's step will go here.
         # For demonstration purposes we will print the agent's unique_id
         print("Hi, I am agent " + str(self.unique_id) +
-              " - TIME: " + str(self.permanence_time) + ".")
+              " - TIME: " + str(self.permanence_time) +
+              " - POSITION: " + str(self.pos) + ".")
+        if(self.spawn):
+            self.spawn = False
+            return
+        self.model.grid.move_agent(self, self.decide_destination())
+
+    def decide_destination(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center=True)
+        print(possible_steps)
+
+        min_cell = {}
+        for key, value in self.model.floor_fields.items():
+            #print(possible_steps[np.argmin(np.take(value, possible_steps))])
+            min_cell[key] = possible_steps[np.argmin(
+                np.take(value, possible_steps))]
+        print(min(min_cell.values()))
+        return min(min_cell.values())
 
 
 class SupermarketModel(Model):
@@ -59,35 +81,29 @@ class SupermarketModel(Model):
                 if (cell in ['A', 'B', 'C', 'D', 'E']):
                     self.entry_points.append((j, i, cell))
 
-        worldMatrix = np.matrix(self.world)
+        worldMatrix = np.matrix(self.world[::-1])
         self.distanceMatrix = np.zeros((self.height, self.width))
         self.distanceMatrix[worldMatrix == 'X'] = np.inf
-        # self.distanceMatrix[worldMatrix == 'A'] = 1
-        # self.distanceMatrix[worldMatrix == 'B'] = 1
-        # self.distanceMatrix[worldMatrix == 'C'] = 1
-        # self.distanceMatrix[worldMatrix == 'D'] = 1
-        # self.distanceMatrix[worldMatrix == 'E'] = 1
+
         self.distanceMatrix[worldMatrix == '1'] = np.inf
         self.distanceMatrix[worldMatrix == '2'] = np.inf
         self.distanceMatrix[worldMatrix == '3'] = np.inf
         self.distanceMatrix[worldMatrix == '4'] = np.inf
         self.distanceMatrix[worldMatrix == '5'] = np.inf
-        print(self.distanceMatrix)
 
-        floor_fields = {}
+        # self.distanceMatrix[worldMatrix == 'A'] = 1
+        # self.distanceMatrix[worldMatrix == 'B'] = 1
+        # self.distanceMatrix[worldMatrix == 'C'] = 1
+        # self.distanceMatrix[worldMatrix == 'D'] = 1
+        # self.distanceMatrix[worldMatrix == 'E'] = 1
+
+        # print(self.distanceMatrix)
+
+        self.floor_fields = {}
         for dest_y, dest_x, dest_label in self.cash_registers:
-            floor_fields[dest_label] = self.calculate_floor_field((dest_x, dest_y - 1))
-
-
-    def calculate_floor_field(self, target):
-        field = self.distanceMatrix.copy()
-
-        for x1 in range(len(field)):
-            for y1 in range(len(field[x1])):
-                if not np.isinf(field[x1,y1]):
-                    field[x1, y1] = distance.euclidean([x1,y1], target)
-
-        return field
+            self.floor_fields[dest_label] = self.calculate_floor_field(
+                (dest_x, dest_y - 1))
+        print(self.floor_fields['1'])
 
     def step(self):
         print("STEP - " + str(len(self.schedule.agents)))
@@ -106,3 +122,13 @@ class SupermarketModel(Model):
         a = CustomerAgent(self.agents_count, self)
         self.agents_count += 1
         return a
+
+    def calculate_floor_field(self, target):
+        field = self.distanceMatrix.copy()
+
+        for x1 in range(len(field)):
+            for y1 in range(len(field[x1])):
+                if not np.isinf(field[x1, y1]):
+                    field[x1, y1] = distance.euclidean([x1, y1], target)
+
+        return field

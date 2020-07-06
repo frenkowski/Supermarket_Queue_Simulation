48309bda98439e477f90ed92d65b4ae59f7c1ee1
Davide Marchetti <dvdmarchetti663@gmail.com>
2020-07-06 21:50:24 +0200

    Ignore closed cash registers in destination calculation

--- src/model.py
+++ src/model.py
@@ -61,6 +61,9 @@ class CustomerAgent(Agent):
 
         destinations = []
         for destination, floor_field in self.model.floor_fields.items():
+            if not self.is_cash_register_open(destination):
+                continue
+
             candidates = [floor_field[y, x] for x, y in possible_steps]
 
             x, y = possible_steps[np.argmin(candidates)]
@@ -73,6 +76,9 @@ class CustomerAgent(Agent):
         selected_move = min(destinations, key=lambda x: x['cost'])
         return selected_move['move']
 
+    def is_cash_register_open(self, destination):
+        cash_register_y, cash_register_x = self.model.cash_registers[destination]
+        return self.model.grid[cash_register_y][cash_register_x].open
 
 class SupermarketModel(Model):
     def __init__(self, N, world, width, height):
@@ -84,7 +90,7 @@ class SupermarketModel(Model):
         self.capacity = N
         self.running = True
 
-        self.cash_registers = []
+        self.cash_registers = {}
         self.entry_points = list()
         self.agents_count = 0
 
@@ -95,7 +101,7 @@ class SupermarketModel(Model):
                     self.grid[j][i] = ObstacleAgent(str(i)+str(j), self)
                 if (cell in ['1', '2', '3', '4', '5']):
                     self.grid[j][i] = CashierAgent(str(i)+str(j), self)
-                    self.cash_registers.append((j, i, cell))
+                    self.cash_registers[cell] = (j, i)
                 if (cell in ['A', 'B', 'C', 'D', 'E']):
                     self.entry_points.append((j, i, cell))
 
@@ -116,10 +122,11 @@ class SupermarketModel(Model):
 
         # print(self.distanceMatrix)
 
+        print(self.cash_registers)
+
         self.floor_fields = {}
-        for dest_y, dest_x, dest_label in self.cash_registers:
-            self.floor_fields[dest_label] = self.calculate_floor_field(
-                (dest_x, dest_y - 1))
+        for dest_label, (dest_y, dest_x) in self.cash_registers.items():
+            self.floor_fields[dest_label] = self.calculate_floor_field((dest_x, dest_y - 1))
 
     def step(self):
         print("STEP - " + str(len(self.schedule.agents)))
